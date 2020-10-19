59fa9e7ebd11076900b8ff1fea2d8fe925256bad
Harshit Srivastava <harshit0300@gmail.com>
2020-10-20 01:12:07 +0530

    ui1

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ mongoose.connect("mongodb://localhost/dbmspro",{ useNewUrlParser: true, useUnifi
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req,res)=>{
     res.render("home");
