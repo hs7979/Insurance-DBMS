5f92aea437c417a23c900b726dd3aedd49da3f73
Harshit Srivastava <58990472+hs7979@users.noreply.github.com>
2020-10-27 21:26:47 +0530

    Update index

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var express        = require ("express"),
 
 var app = express();
 
-// mongoose.connect("mongodb://localhost/dbmspro",{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify:false });
+mongoose.connect("mongodb://localhost/dbmspro",{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify:false });
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
